b14d97295112510627627ee17a470005b3d44851
muhammadhasnain112 <muhammadhasnain12446@gmail.com>
2025-02-10 21:06:18 +0500

    To Do List

--- App.js
+++ App.js
@@ -1,58 +1,74 @@
+function trash(){
+    localStorage.clear()
+    ul.innerHTML = ``
+    // arr.clear()
+}
+// localStorage.clear()
 let input = document.getElementById("input")
 let ul = document.getElementById("ul")
 let add = document.getElementById("add")
-let keys
-let li
+// let li
+let uper
+let arr = JSON.parse(localStorage.getItem('todo')) || [];
 
-add.addEventListener('click', build)
-function build(){
-    let uper = input.value.slice(0, 1).toUpperCase() + input.value.slice(1)
-    li = document.createElement('li')
-    let span = document.createElement('span')
-    
-    span.innerText = uper
-    li.appendChild(span)
-    input.value = ""
-    ul.appendChild(li)
-    let dlt = document.createElement('img')
-    // dlt.innerText = `Delete`
-    dlt.classList.add('img')
-    dlt.setAttribute('src', 'trash.png')
-    dlt.setAttribute('onclick', 'delet(this)')
-    li.appendChild(dlt)
-    let edit = document.createElement('img')
-    // edit.innerText = `Edi`
-    edit.classList.add('img')
-    edit.setAttribute('src', 'edit.png')
-    edit.setAttribute('onclick', 'edit(this)')
-    li.appendChild(edit)
-    
-    let hr = document.createElement('hr')
-    li.appendChild(hr)
-}
 document.addEventListener('keydown', function (event) {
-    keys = event.key
-    // console.log(keys);
-    
-    // console.log(keys);
-    if(keys == "Enter"){
-        // console.log("ll");
-        build()
-        
+    let keys = event.key
+    if (keys == "Enter") {
+        show()
     }
 })
-
-function delet(a) {
-let ab = a.parentElement
-// console.log(ab);
-
-    ab.remove()
-
+for (let i = 0; i < arr.length; i++) {
+    ul.innerHTML += `<li><span>${arr[i].list}</span><img class="img clear" src="trash.png" ><img class="img" src="edit.png" onclick="edit(this)"><hr></li>`
+    let clear = document.querySelectorAll('.clear')
+    for (let j = 0; j < clear.length; j++) {
+        clear[j].addEventListener('click', (e) => {
+            let parentElement = e.target.parentElement
+            console.log(parentElement);
+            parentElement.remove()
+            arr.splice(j, 1)
+            localStorage.setItem('todo', JSON.stringify(arr));
+        })
+    }
 }
+add.addEventListener('click', show)
+function show() {
+    uper = input.value.slice(0, 1).toUpperCase() + input.value.slice(1)
+    let obj = {
+        list : uper,
+    }
+    input.value = ``
+    arr.push(obj)
+    localStorage.setItem('todo', JSON.stringify(arr))
+    ul.innerHTML = ``
+    // console.log(arr[2]);
+    for (let i = 0; i < arr.length; i++) {
+        ul.innerHTML += `<li><span>${arr[i].list}</span><img class="img clear" src="trash.png" ><img class="img" src="edit.png" onclick="edit(this)"><hr></li>`
+        // ul.innerHTML = ``
+    }
+    let clear = document.querySelectorAll('.clear')
+    for (let i = 0; i < clear.length; i++) {
+        clear[i].addEventListener('click', (e) => {
+            let parentElement = e.target.parentElement
+            // console.log(parentElement);
+            parentElement.remove()
+            arr.splice(i, 1)
+            localStorage.setItem('todo', JSON.stringify(arr));
+        })
+    }
+}
+
+
+// document.getElementById('trash').addEventListener('click',()=>{
+    // location.reload
+// })
+
+
 function edit(abc) {
     let li = abc.parentElement.firstChild
+    // console.log(abc.parentElement.firstChild);
     let pr = prompt("Enter Your Text")
     li.innerText = pr.slice(0, 1).toUpperCase() + pr.slice(1)
 }
 
-
+// function cl(){
+// }
